f66497b65fe790934894a532de0944b5d6f39cf6
thanu <thanumalyan.t@gmail.com>
2026-08-17 12:15:01 +0530

    .

--- CHAPTER5/CH5_PROJECT5.c
+++ CHAPTER5/CH5_PROJECT5.c
@@ -18,12 +18,12 @@ int main(void)
     printf("Enter the amount of taxable income: ");
     scanf("%f", &income);
 
-    if (income <= 750) taxDue = income * 0.01;
-    else if (income <= 2250) taxDue = 7.50 + (income - 750) * 0.02; 
-    else if (income <= 3750) taxDue = 37.50 + (income - 2250) * 0.03; 
-    else if (income <= 5250) taxDue = 82.50 + (income - 3750) * 0.04; 
-    else if (income <= 7000) taxDue = 142.50 + (income - 5250) * 0.05; 
-    else taxDue = 230.00 + (income - 7000) * 0.06;
+    if (income <= 750) taxDue = income * 0.01f;
+    else if (income <= 2250) taxDue = 7.50f + (income - 750) * 0.02f; 
+    else if (income <= 3750) taxDue = 37.50f + (income - 2250) * 0.03f; 
+    else if (income <= 5250) taxDue = 82.50f + (income - 3750) * 0.04f; 
+    else if (income <= 7000) taxDue = 142.50f + (income - 5250) * 0.05f; 
+    else taxDue = 230.00f + (income - 7000) * 0.06f;
 
     printf("Tax due: %.2f", taxDue);
 
