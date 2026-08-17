0a57507a91f79b88b5b4c5571b3b97e2f7608df3
thanu <thanumalyan.t@gmail.com>
2026-08-17 12:03:07 +0530

    added project2(ch9)

---
+++ CHAPTER9/CH9_PROJECT2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+//2. Modify Programming Project 5 from Chapter 5 so that it uses a function to compute the
+//amount of income tax. When passed an amount of taxable income, the function will return
+//the tax due.
+
+//In one state, single residents are subject to the following income tax:
+//Income            Amount of tax
+//Not over $750     1% of income
+//$750-$2,250       $7.50   plus 2% of amount over $750
+//$2,250-$3,750     $37.50  plus 3% of amount over $2,250
+//$3,750-$5,250     $82.50  plus 4% of amount over $3,750
+//$5,250-$7,000     $142.50 plus 5% of amount over $5,250
+//Over $7,000       $230.00 plus 6% of amount over $7,000
+//Write a program that asks the user to enter the amount of taxable income, then displays the
+//tax due.
+
+float tax_due (float taxable_income);
+
+int main (void)
+{
+    float taxable_income;
+
+    printf("\t\t--TAX DUE CALCULATOR--\n\n");
+
+    printf("Enter the amount of taxable income: ");
+    scanf("%f", &taxable_income);
+
+    if (taxable_income < 0) {
+        printf("\nINVALID AMOUNT ENTRY DETECTED!!\nEXITED FROM THE PROGRAM.\n");
+        return 0;
+    }
+
+    printf("Tax Due:\t$%.2f", tax_due(taxable_income));
+
+    return 0;
+}
+
+float tax_due (float income) 
+{
+    float tax;
+
+    if (income <= 750) tax = income * 0.01;
+    else if (income <= 2250) tax = 7.50 + (income - 750) * 0.02;
+    else if (income <= 3750) tax = 37.50 + (income - 2250) * 0.03;
+    else if (income <= 5250) tax = 82.50 + (income - 3750) * 0.04;
+    else if (income <= 7000) tax = 142.50 + (income - 5250) * 0.05;
+    else tax = 230.00 + (income - 7000) * 0.06;
+
+    return tax;
+}
