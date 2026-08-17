5c7e45323c264afc0f418db9cd2245e98c413c4c
thanu <thanumalyan.t@gmail.com>
2026-08-17 12:17:30 +0530

    change double to float

--- CHAPTER9/CH9_PROJECT2.c
+++ CHAPTER9/CH9_PROJECT2.c
@@ -40,12 +40,12 @@ float tax_due (float income)
 {
     float tax;
 
-    if (income <= 750) tax = income * 0.01;
-    else if (income <= 2250) tax = 7.50 + (income - 750) * 0.02;
-    else if (income <= 3750) tax = 37.50 + (income - 2250) * 0.03;
-    else if (income <= 5250) tax = 82.50 + (income - 3750) * 0.04;
-    else if (income <= 7000) tax = 142.50 + (income - 5250) * 0.05;
-    else tax = 230.00 + (income - 7000) * 0.06;
+    if (income <= 750) tax = income * 0.01f;
+    else if (income <= 2250) tax = 7.50f + (income - 750) * 0.02f;
+    else if (income <= 3750) tax = 37.50f + (income - 2250) * 0.03f;
+    else if (income <= 5250) tax = 82.50f + (income - 3750) * 0.04f;
+    else if (income <= 7000) tax = 142.50f + (income - 5250) * 0.05f;
+    else tax = 230.00f + (income - 7000) * 0.06f;
 
     return tax;
 }
